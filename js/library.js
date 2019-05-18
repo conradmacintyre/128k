55cf26e24499d37b108f629cc8d2c5850fadd328
Conrad MacIntyre <conrad@128k.ca>
2019-05-18 16:18:11 -0700

    Billboard v1.0

--- js/library.js
+++ js/library.js
@@ -1,3 +1,32 @@
 // library.js
 
 // Billboard
+function initBillboard() {
+	const billboards = document.querySelectorAll('[data-billboard]');
+	billboards.forEach( billboard => {
+		const slides = [...billboard.getElementsByClassName('billboard-slide')];
+		const interval = parseInt(billboard.getAttribute('data-billboard')) * 1000;
+		slides.forEach( slide => {
+			let bgImg = slide.getAttribute('data-bgimg');
+			slide.style.backgroundImage = 'url('+bgImg+')'
+		});
+		cycleActiveItem(slides,interval);
+	});
+} initBillboard();
+
+function cycleActiveItem( array, interval ) {
+	function cycle( array ) {
+		for (let i=0; i<array.length; i++) {
+			if ( array[i].classList.contains('active') ) {
+				array[i].classList.remove('active');
+				if ( i == array.length - 1 ) {
+					array[0].classList.add('active');
+				} else {
+					array[i + 1].classList.add('active');
+				}
+				break;
+			}
+		}
+	}
+	let cycleTimer = setInterval(() => cycle( array ), interval);
+}
